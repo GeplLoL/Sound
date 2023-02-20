44903b698a2c1e8cd0157f6f131858a05a49809b
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-02-20 14:32:18 +0200

    Add files via upload

---
+++ tooFailiga/Funktsion.py
@@ -0,0 +1,37 @@
+﻿def loe_failist(fail:str)->str:
+    """
+    loeme failist
+    """
+    f=open(fail,"r",encoding="utf-8-sig")
+    jarjend=[] 
+    for rida in f:
+        jarjend.append(rida.strip())
+    f.close()
+    return jarjend
+def Kirjuta_failisse(fail:str,jarjend:list):
+    """
+    salvestame
+    """
+    f=open(fail,"a", encoding="utf-8-sig")
+    for line in jarjend:
+        f.write(line+"\n")
+    f.close()
+from gtts import gTTS
+import os
+def Heli(text:str,keel:str):
+    obj=gTTS(text=text,lang=keel,slow=False).save("heli.mp3") 
+    os.system("heli.mp3")
+def loe_failist(f):
+    fail=open(f,'r',encoding="utf-8-sig")
+    mas=[]
+    for rida in fail:
+        mas.append(rida.strip())
+    fail.close()
+    return mas
+def tõlk(text:str,keel:str):
+    vastus=input("Sisse sõna: ")
+    if vastus in text("SõnastikET.txt"):
+        vastus.index(text)
+        obj=gTTS(text=text,lang=keel,slow=False).save("heli1.mp3")
+        os.system("heli1.mp3")
+
---
+++ tooFailiga/tooFailiga.py
@@ -0,0 +1,23 @@
+﻿from Funktsion import *
+laused=[]
+eesti=[]
+vene=[]
+while True:
+    v=int(input("1-loeme failist\n2-salvestame\n3-kuulamine\n4-tõlk"))
+    if v==1:
+        laused=loe_failist("Laused.txt")
+        for line in laused:
+            print(line)
+    elif v==2:
+        line=input("Lisa laused: ")
+        laused.append(line)
+        Kirjuta_failisse("Laused.txt", laused)
+    elif v==3:
+        text=""
+        for line in laused:
+            text=text+" "+line
+        Heli(text, "et")
+    elif v==4:
+        text=""
+        for line in laused:
+            tõlk(text,"ru")
